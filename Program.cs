2535dd7168d696fa28000546f93613a3b9ee1a40
amirhoseinsazizavareh <158080807+amirhoseinsazizavareh@users.noreply.github.com>
2024-01-29 20:55:53 +0330

    Add files via upload
    امیرحسین سازی زواره

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number;
+
+       
+        Console.Write("Please enter a four-digit number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+       
+        int sum = 0;
+        int tempNumber = number;
+        while (tempNumber > 0)
+        {
+            int digit = tempNumber % 10;
+            sum += digit;
+            tempNumber /= 10;
+        }
+
+       
+        Console.WriteLine("The sum of the digits of the entered number: " + sum);
+    }
+}
